de93a0770eb120490c75d054be7236013c1046dd
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-21 15:58:18 +0000

    condição encadeada

--- encadeada e múltiplas cond.c
+++ encadeada e múltiplas cond.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
+int main(){
+int idade;
+printf("***Testando condição encadeada.***\n\n");
 
+printf("Digite a idade da pessoa.\n");
+scanf("%d",&idade);
+
+if (idade < 12)
+{
+   printf("Você é uma criança.\n");
+}
+else if (idade >= 12 && idade <= 18)
+{
+    printf ("Você é um adolescente.\n");
+}
+else if ( idade >= 18 && idade <= 60)
+{
+    printf("Você é um adulto.");
+} else if (idade < 60 ){
+
+}
+
+
+return 0;
+}
