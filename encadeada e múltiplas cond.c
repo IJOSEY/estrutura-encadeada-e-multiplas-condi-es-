3f5e51ed8122298737361f6286391791d1a3834e
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-22 13:23:33 +0000

    aprimorando

--- encadeada e múltiplas cond.c
+++ encadeada e múltiplas cond.c
@@ -8,18 +8,18 @@ scanf("%d",&idade);
 
 if (idade < 12)
 {
-   printf("Você é uma criança.\n");
+   printf("Você tem %d anos é uma criança.\n",idade);
 }
 else if (idade >= 12 && idade <= 18)
 {
-    printf ("Você é um adolescente.\n");
+    printf ("Você tem %d  anos é um adolescente.\n",idade);
 }
 else if ( idade >= 18 && idade <= 60)
 {
-    printf("Você é um adulto.\n");
+    printf("Você tem %d anos é um adulto.\n",idade);
 
 } else if (idade < 60 ){
-    printf("Você é idoso.\n");
+    printf("Você tem %d anos é um idoso.\n",idade);
 
 }
 
