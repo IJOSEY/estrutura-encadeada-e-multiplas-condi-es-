693c0f6b11c1924b37e4a8a5bb3925085fbb0f3c
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-22 15:04:35 +0000

    Finalizado código e alinhadas condições

--- beneficiosocial.c
+++ beneficiosocial.c
@@ -6,15 +6,28 @@ float renda;
 
 printf("Verificação de benefício.\n\n");
 
-printf("Informe a sua idade.");
+printf("Informe a sua idade:");
 scanf("%d",&idade);
 
-printf("Quanto depedentes possui.");
+printf("Quanto depedentes possui:");
 scanf("%d",&dependentes);
 
-printf("Iforme a sua renda.");
+printf("Iforme a sua renda:");
 scanf("%f",&renda);
-if
+if(idade >= 18 && idade < 65 ){
+    if(renda < 3000 ){
+        if(dependentes > 2 ){
+            printf("Você tem direito ao benfício!!.\n");
+
+        }else {
+            printf("Numero de dependentes não atende ao critério.\n");
+        }
+    } else{
+        printf("Renda não atende ao critério.\n");
+    }
+} else{
+    printf("Idade não atende aos critérios.\n");
+}
 
 return 0;
 }
