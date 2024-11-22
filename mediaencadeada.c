e7e11ef31679769d9d0575828d3d4551c574c514
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-22 13:59:46 +0000

    Condição dasnotas executadas com sucesso

--- mediaencadeada.c
+++ mediaencadeada.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+int main(){
+    float nota;
+    printf("Qual conceito sua nota se enquadra.\n\n");
+
+    printf("Digite a sua nota?\n");
+    scanf("%f",&nota);
+
+    if(nota >= 9.0 ){
+         printf("Sua nota tem conceito A");
+
+    }else if(nota >= 8.0 && nota < 9.0 ){
+        printf("Sua nota tem conceito B");
+
+    }else if (nota >= 7.0 && nota < 8.0 ){
+        printf("Sua nota tem conceito C");
+
+    }else if (nota >= 6.0 && nota < 7.0  ){
+        printf("Sua nota tem conceito D");
+
+    }else{
+        printf("Sua nota tem conceito E");
+
+    }
+
+    return 0;
+}
