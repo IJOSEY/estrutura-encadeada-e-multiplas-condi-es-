1ba26817d43aefabcb7ea165b6b5160e44aff3e0
IJOSEY <ijoseybs@GMAIL.COM>
2024-11-22 14:46:42 +0000

    Começando a estabelecer parametros e condições

--- beneficiosocial.c
+++ beneficiosocial.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+
 int main() {
-    
+int idade, dependentes;
+float renda;
+
+printf("Verificação de benefício.\n\n");
+
+printf("Informe a sua idade.");
+scanf("%d",&idade);
+
+printf("Quanto depedentes possui.");
+scanf("%d",&dependentes);
+
+printf("Iforme a sua renda.");
+scanf("%f",&renda);
+if
+
+return 0;
 }
